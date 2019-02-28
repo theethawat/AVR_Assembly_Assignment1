62ce779ac99a79edd023fe12659eeb7dcd8a5c4d
Theethawat <theethawat.s@outlook.co.th>
2019-02-28 23:42:38 +0700

    Initial commit

---
+++ AssemblerApplication1/main.asm
@@ -0,0 +1,72 @@
+;
+; AssemblerApplication1.asm
+;
+; Created: 2/26/2019 10:16:50 PM
+; Author : theethawat
+; Thank you : https://www.instructables.com/id/Command-Line-Assembly-Language-Programming-for-Ard/
+
+.INCLUDE "m328Pdef.inc"
+.DEF VAR_Al = r16
+.DEF VAR_Ah = r17
+.DEF VAR_Bl = r18
+.DEF VAR_Bh = r19
+.DEF Temp = r20
+
+.CSEG ; Code
+		; 1 = out 0 = in  F=1111 FF=11111111
+		;7 6 5 4 3 2 1 0 is the port of PB
+		;i want to use port PB0,PB1,PB2,PB3 as input
+
+		;config input port
+.ORG    0x00
+		rjmp Start
+
+Start:	ldi VAR_Al,0xfe ; PB0
+		out DDRB,VAR_Al
+	
+		ldi VAR_Ah,0xfd ; PB1
+		out DDRB,VAR_Ah
+
+		ldi VAR_Bl,0xfc ; PB2
+		out DDRB,VAR_Bl
+	
+		ldi VAR_Bh,0xfb ; PB3
+		out DDRB,VAR_Bh
+
+		ldi Temp,0b00000011 ;PC0
+		out DDRC,Temp
+		out PortC,Temp
+		
+
+		in VAR_Ah,PinB
+		in VAR_Al,PinB
+		in VAR_Bh,PinB
+		in VAR_Bl,PinB
+
+		add VAR_Al,VAR_Bl
+		adc VAR_Ah,VAR_Bh
+		
+		cpi VAR_Al,1
+		
+
+
+
+.DSEG ; Data
+.ESEG ; EEprom
+
+		
+
+		
+		;out DDRC,Output2
+
+		
+		;out DDRC,Output3
+
+		;
+		;out DDRC,Output4
+;DDRB stands for "Data Direction Register B" and 
+	;it sets up the "pins" on PortB. On the pinout map for the 328p
+	; you can see that there are 8 pins labeled PB0, PB1, ... , PB7.
+; when we load the binary number 00100000 into the DDRB register 
+	;we are saying that we want PB0, PB1, PB2, PB3, PB4, PB6, and PB7 set as INPUT pins 
+	;since they have 0's in them, and PB5 is set as an OUTPUT pin since we put a 1 in that spot.
